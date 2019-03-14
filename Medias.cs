4d283366166697f00c78cd362e4997ab6e939ade
dcl13 <dcl13@iMac-de-iMac13.local>
2019-03-14 10:23:28 -0600

    Problema 00

--- Medias.cs
+++ Medias.cs
@@ -13,10 +13,22 @@ namespace AutomatizarPruebasUnitarias {
          */
         public static double mediaAritmetica(params int[] vals) { }
 
+ int i,b=0,num=5;
+         double suma=0.0;
+           Console.WriteLine("Para salir ingresa cero");
+          while(num!=0){
+               Console.WriteLine("Ingresa un numero");
+               num = Convert.ToInt16(Console.ReadLine());
+              suma=suma+num;
+              b++;
+          }
+         Console.WriteLine(suma/(b-1));
+        Console.ReadLine();
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
         private static double raizEnesima(double x, int n) { }
+
 
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
