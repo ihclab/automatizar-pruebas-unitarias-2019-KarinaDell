346167431c4c8e9bd5bfd6c41adc158d3c9b4132
dcl13 <dcl13@iMac-de-iMac13.local>
2019-03-19 10:39:01 -0600

    Problema 01

--- Medias.cs
+++ Medias.cs
@@ -11,33 +11,42 @@ namespace AutomatizarPruebasUnitarias {
         /**
          * Calcula y regresa la media artimética
          */
-        public static double mediaAritmetica(params int[] vals) { }
+        public static double mediaAritmetica(params int[] vals) { 
 
- int i,b=0,num=5;
-         double suma=0.0;
-           Console.WriteLine("Para salir ingresa cero");
+         int i,b=0,num=5;
+         double suma=0.0
           while(num!=0){
                Console.WriteLine("Ingresa un numero");
                num = Convert.ToInt16(Console.ReadLine());
               suma=suma+num;
               b++;
           }
-         Console.WriteLine(suma/(b-1));
+        for (i=1; i>=0; i++) {
+         Console.WriteLine('000' + i + ':media aritmética: ' suma/(b-1));
         Console.ReadLine();
+          }
+        }
         /**
          * Calcula y regresa la raíz enésima = x^(1/n)
          */
-        private static double raizEnesima(double x, int n) { }
+        private static double raizEnesima(double x, int n) {
 
 
+         }
         /**
          *  Usa raizEnesima para calcular y regresar la media geométrica
          */
-        public double mediaGeometrica(params int[] vals) { }
+        public double mediaGeometrica(params int[] vals) {
+            Console.WriteLine("Ingresa un numero");
+            num = Convert.ToInt16(Console.ReadLine());
+         }
 
         /**
          * Este método no está implementado.
          */
-        public static double mediaArmonica(params int[] vals) { }
+        public static double mediaArmonica(params int[] vals) { 
+            //Poner excepción
+             Console.WriteLine("Este método no está implementado");
+        }
     }
 }
